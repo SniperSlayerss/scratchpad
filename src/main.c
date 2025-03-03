b24a971ecf2fe2cf4bee628910caa37bfe619697
Jack Baker <sniperslayer122@gmail.com>
2025-03-03 22:46:40 +0000

    feat: sorta new lines

--- src/main.c
+++ src/main.c
@@ -5,6 +5,7 @@
 #include <SDL3/SDL_surface.h>
 #include <SDL3_ttf/SDL_ttf.h>
 
+#include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -13,44 +14,54 @@
 #define HEIGHT 800
 #define BUFFER_SIZE 256
 
-const int pixel_size = 37;
-int y = 0;
 char text_buffer[BUFFER_SIZE] = {0};
+char text_row_buffer[BUFFER_SIZE] = {0};
+int font_height;
+int y = 0;
 
-SDL_Window *window     = NULL;
+SDL_Window *window = NULL;
 SDL_Renderer *renderer = NULL;
-TTF_Font *font         = NULL;
+TTF_Font *font = NULL;
+TTF_TextEngine *text_engine = NULL;
 
-int init() {
+bool init() {
   if (!SDL_Init(SDL_INIT_VIDEO)) {
     SDL_Log("SDL_INIT Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   if (!TTF_Init()) {
     SDL_Log("TTF_INIT Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
-  window = SDL_CreateWindow("Scratchpad", WIDTH, HEIGHT, 0);
+  window = SDL_CreateWindow("Scratchpad", WIDTH, HEIGHT,
+                            SDL_WINDOW_HIGH_PIXEL_DENSITY);
   if (window == NULL) {
     SDL_Log("SDL_CreateWindow Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
   renderer = SDL_CreateRenderer(window, NULL);
   if (renderer == NULL) {
     SDL_Log("SDL_CreateRenderer Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
-  font = TTF_OpenFont("fonts/FiraCodeNerdFontMono-Regular.ttf", 28);
+  font = TTF_OpenFont("fonts/FiraCodeNerdFontMono-Regular.ttf", 36);
   if (font == NULL) {
     SDL_Log("TTF_OpenFont Error: %s\n", SDL_GetError());
-    return 0;
+    return false;
   }
 
-  return 1;
+  text_engine = TTF_CreateRendererTextEngine(renderer);
+  if (text_engine == NULL) {
+    SDL_Log("TTF_CreateSurfaceTextEngine Error: %s\n", SDL_GetError());
+    return false;
+  }
+
+  font_height = TTF_GetFontHeight(font);
+  return true;
 }
 
 void append_char(char c) {
@@ -65,7 +76,7 @@ void append_char(char c) {
 
 char pop_char() {
   size_t len = strlen(text_buffer);
-  if (len == 0 ) {
+  if (len == 0) {
     return '\0';
   }
 
@@ -74,21 +85,31 @@ char pop_char() {
   return c;
 }
 
-void render_text(const char *text, SDL_Color text_color) {
-  size_t len = strlen(text_buffer);
+void render_text(const char *raw_text, SDL_Color text_color) {
+  size_t len = strlen(raw_text);
   if (len == 0) {
     return;
   }
 
-  // New line sort out test
-  SDL_Surface *surface = TTF_RenderText_Blended(font, text, len, text_color);
-  SDL_Texture *texture = SDL_CreateTextureFromSurface(renderer, surface);
-  SDL_FRect dstrect = {0, pixel_size * y, (float) surface->w, pixel_size};
-  SDL_RenderTexture(renderer, texture, NULL, &dstrect);
+  int i = 0;
+  int row = 0;
+  int col = 0;
+  TTF_Text *text = {};
+  while (i < BUFFER_SIZE && i < len + 1) {
+    if (text_buffer[i] == '\n' || text_buffer[i] == '\0') {
+      text = TTF_CreateText(text_engine, font, text_row_buffer, strlen(text_row_buffer));
+      TTF_DrawRendererText(text, 0, row);
+      col = 0;
+      ++row;
+    }
 
-  // draw to screen
-  SDL_DestroySurface(surface);
-  SDL_DestroyTexture(texture);
+    text_row_buffer[col] = text_buffer[i];
+    text_row_buffer[col+1] = '\0';
+    ++col;
+    ++i;
+  }
+
+  TTF_DestroyText(text);
 }
 
 int main(int argc, char *argv[]) {
@@ -97,7 +118,7 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  _Bool done = false;
+  bool done = false;
   while (!done) {
     SDL_Event event;
     while (SDL_PollEvent(&event)) {
@@ -111,27 +132,27 @@ int main(int argc, char *argv[]) {
 
       if (event.type == SDL_EVENT_KEY_DOWN) {
         switch (event.key.key) {
-          case SDLK_UP:
-            if (y > 0){
-              y -= 1;
-            }
-            break;
-          case SDLK_DOWN:
-            if (y < HEIGHT / pixel_size - 1) {
-              y += 1;
-            }
-            break;
-          case SDLK_BACKSPACE:
-            pop_char();
-            break;
-          case SDLK_KP_ENTER:
-            append_char('\n');
-          case SDLK_I:
-            SDL_StartTextInput(window);
-            break;
-          case SDLK_ESCAPE:
-            SDL_StopTextInput(window);
-            break;
+        case SDLK_UP:
+          if (y > 0) {
+            y -= 1;
+          }
+          break;
+        case SDLK_DOWN:
+          if (y < HEIGHT / font_height - 1) {
+            y += 1;
+          }
+          break;
+        case SDLK_BACKSPACE:
+          pop_char();
+          break;
+        case SDLK_1:
+          append_char('\n');
+        case SDLK_I:
+          SDL_StartTextInput(window);
+          break;
+        case SDLK_ESCAPE:
+          SDL_StopTextInput(window);
+          break;
         }
       }
     }
@@ -142,7 +163,6 @@ int main(int argc, char *argv[]) {
 
     SDL_Color text_color = {224, 222, 244};
     render_text(text_buffer, text_color);
-
 
     SDL_RenderPresent(renderer);
   }
